22da50e1f528a8e819499901f988c112fc26c2c0
Oleksandr Tereshchenko <toporok@gmail.com>
2024-03-18 10:24:30 +0200

    first classes

---
+++ promisegraph/core.py
@@ -0,0 +1,35 @@
+"""
+This module will later be split into multiple modules.
+"""
+import hashlib
+import json
+from functools import cached_property
+
+from pydantic import BaseModel, ConfigDict
+
+
+class Node(BaseModel):
+    """
+    TODO Oleksnadr: update this docstring a bit ?
+    A base class for immutable pydantic objects. It is frozen and has a git-style hash key that is calculated from the
+    JSON representation of the object.
+    """
+
+    model_config = ConfigDict(frozen=True, extra="allow")
+
+    @cached_property
+    def hash_key(self) -> str:
+        """
+        Get the hash key for this object. It is a hash of the JSON representation of the object.
+        """
+        return hashlib.sha256(
+            json.dumps(self.model_dump(), ensure_ascii=False, sort_keys=True).encode("utf-8")
+        ).hexdigest()
+
+
+class Promise:
+    pass
+
+
+class PromisePath:
+    pass
