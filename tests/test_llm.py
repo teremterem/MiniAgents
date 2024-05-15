4c05ea962a07518053cb881ad3b15c7720652c72
Oleksandr Tereshchenko <toporok@gmail.com>
2024-05-15 09:45:41 +0300

    test_llm.py

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -1,39 +1,68 @@
+# noinspection PyUnresolvedReferences
 """
-Code example for using LLMs.
+Test the LLM agents.
 """
 
-# noinspection PyUnresolvedReferences
-import readline  # pylint: disable=unused-import
+from typing import Callable
 
 import pytest
 from dotenv import load_dotenv
 
+from miniagents.messages import Message
+from miniagents.miniagents import MiniAgents, MiniAgent
+
 load_dotenv()
 
 # pylint: disable=wrong-import-position
-# from miniagents.ext.llm.anthropic import create_anthropic_agent
+from miniagents.ext.llm.anthropic import create_anthropic_agent
 from miniagents.ext.llm.openai import create_openai_agent
-from miniagents.miniagents import MiniAgents
-
-# llm_agent = create_anthropic_agent(model="claude-3-haiku-20240307")
-llm_agent = create_openai_agent(model="gpt-3.5-turbo-0125")
-
-mini_agents = MiniAgents()
 
 
+def _check_openai_response(message: Message) -> None:
+    assert message.text.strip() == "I AM ONLINE"
+    assert message.openai.choices[0].finish_reason == "stop"
+
+
+def _check_anthropic_response(message: Message) -> None:
+    assert message.text.strip() == "I AM ONLINE"
+    assert message.anthropic.stop_reason == "end_turn"
+
+
+@pytest.mark.parametrize(
+    "llm_agent, check_response_func",
+    [
+        (create_openai_agent(model="gpt-3.5-turbo-0125"), _check_openai_response),
+        (create_anthropic_agent(model="claude-3-haiku-20240307"), _check_anthropic_response),
+    ],
+)
 @pytest.mark.asyncio
-async def test_llm() -> None:
-    async with mini_agents:
+@pytest.mark.parametrize("stream", [False, True])
+@pytest.mark.parametrize("schedule_immediately", [False, True])
+async def test_llm(
+    schedule_immediately: bool,
+    stream: bool,
+    llm_agent: MiniAgent,
+    check_response_func: Callable[[Message], None],
+) -> None:
+    """
+    Assert that all the LLM agents can respond to a simple prompt.
+    """
+    async with MiniAgents(schedule_immediately_by_default=schedule_immediately):
         reply_sequence = llm_agent.inquire(
-            "How are you today?",
-            max_tokens=1000,
-            temperature=0.0,
-            system="Respond only in Yoda-speak.",
+            Message(text="ANSWER:", role="assistant"),
+            system=(
+                "This is a test to verify that you are online. Your response will be validated using a strict "
+                "program that does not tolerate any deviations from the expected output at all. Please respond "
+                "with these exact words, all capitals and no punctuation: I AM ONLINE"
+            ),
+            stream=stream,
+            max_tokens=20,
+            temperature=0,
         )
 
-        print()
+        result = ""
         async for msg_promise in reply_sequence:
             async for token in msg_promise:
-                print(token, end="", flush=True)
-            print()
-            print()
+                result += token
+            check_response_func(await msg_promise.acollect())
+    assert result.strip() == "I AM ONLINE"
