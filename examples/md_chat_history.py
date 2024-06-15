66dd6ef60e74e440fc629f32f21d786fbe850a24
Oleksandr Tereshchenko <toporok@gmail.com>
2024-06-16 01:21:42 +0300

    md_chat_history.py

--- examples/md_chat_history.py
+++ examples/md_chat_history.py
@@ -3,9 +3,12 @@ Parse a chat history in markdown format (work in progress).
 TODO Oleksandr: this is a temporary example that will be deleted when the "history-in-md" utility is implemented.
 """
 
+from pathlib import Path
 from pprint import pprint
 
 from markdown_it import MarkdownIt
+
+CHAT_MD_FILE = Path(__file__).parent / "../../talk-about-miniagents/CHAT.md"
 
 
 def main() -> None:
@@ -13,20 +16,14 @@ def main() -> None:
     Main function.
     """
     md = MarkdownIt()
-    text = """\
-user
----------------
-hello hello
-how are
-- you
-doing
-
-assistant
----------------
-fine
-"""
-    tokens = md.parse(text)
+    tokens = md.parse(CHAT_MD_FILE.read_text(encoding="utf-8"))
+    print()
+    print()
+    print()
     pprint(tokens)
+    print()
+    print()
+    print()
 
 
 if __name__ == "__main__":
