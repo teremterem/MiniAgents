f68bd89a07dc07f11cf7ddaca6854c901c4c307e
Oleksandr Tereshchenko <toporok@gmail.com>
2024-06-17 22:25:03 +0300

    examples/DELETEME/md_chat_history.py

--- examples/DELETEME/md_chat_history.py
+++ examples/DELETEME/md_chat_history.py
@@ -3,45 +3,75 @@ Parse a chat history in markdown format (work in progress).
 TODO Oleksandr: this is a temporary example that will be deleted when the "history-in-md" utility is implemented.
 """
 
+from dataclasses import dataclass
 from pathlib import Path
-from pprint import pprint
+from typing import Optional
 
 from markdown_it import MarkdownIt
 
-# CHAT_MD_FILE = Path(__file__).parent / "../../../talk-about-miniagents/CHAT.md"
-CHAT_MD_FILE = Path(__file__).parent / "sample.md"
+from miniagents.messages import Message
+
+CHAT_MD_FILE = Path(__file__).parent / "../../../talk-about-miniagents/CHAT.md"
+
+md = MarkdownIt()
+
+
+@dataclass
+class Section:
+    """
+    Represents a section of the markdown content.
+    """
+
+    heading: str
+    content_start_line: int
+    content: Optional[str] = None
+
+
+def parse_md_dialog(md_content: str) -> list[Message]:
+    """
+    Parse a markdown content as a dialog.
+    """
+    md_lines = md_content.split("\n")
+    md_tokens = md.parse(md_content)
+
+    last_section: Optional[Section] = None
+    sections: list[Section] = []
+
+    for idx, md_token in enumerate(md_tokens):
+        if md_token.type != "heading_open" or md_token.tag != "h1" or md_token.level != 0:
+            continue
+
+        if last_section:
+            last_section.content = "\n".join(md_lines[last_section.content_start_line : md_token.map[0]])
+            sections.append(last_section)
+
+        last_section = Section(
+            heading=md_tokens[idx + 1].content,  # the next token is `inline` with the heading content
+            content_start_line=md_token.map[1],
+        )
+
+    if last_section:
+        last_section.content = "\n".join(md_lines[last_section.content_start_line :])
+        sections.append(last_section)
+
+    # TODO Oleksandr: keep only the ones that start with "user" or "assistant"
+    # TODO Oleksandr: cut off leading empty lines
+    # TODO Oleksandr: cut off trailing whitespaces
+    # TODO Oleksanr: skip empty sections
+    messages = [Message(text=section.content, role=section.heading) for section in sections]
+    return messages
 
 
 def main() -> None:
     """
     Main function.
     """
-    md = MarkdownIt()
     md_content = CHAT_MD_FILE.read_text(encoding="utf-8")
-    md_tokens = md.parse(md_content)
-    print()
-    print()
-    print()
-    for md_token in md_tokens:
-        if md_token.type == "heading_open":
-            print()
-            print()
-            print()
-        children = md_token.children
-        md_token.children = None
-        pprint(md_token)
-        if md_token.type not in ("heading_open", "heading_close") and children:
-            for child in children:
-                print(" ", child)
-        if md_token.type == "heading_close":
-            print()
-            print()
-            print()
+
+    for message in parse_md_dialog(md_content):
+        print(repr(message.role))
+        print(repr(message.text[:100]))
         print()
-        # print(token.map, token.type, token.tag, token.level, "-----", token.content[:50], "-----")
-    print()
-    print()
-    print()
 
 
 if __name__ == "__main__":
