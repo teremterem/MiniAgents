d073849640c27c37c738d39770f15a306abb06da
Oleksandr Tereshchenko <toporok@gmail.com>
2024-06-19 09:51:04 +0300

    miniagents/utils.py::adialog_loop()

--- examples/conversation.py
+++ examples/conversation.py
@@ -10,8 +10,7 @@ from dotenv import load_dotenv
 from miniagents.ext.console.console_user_agent import create_console_user_agent
 from miniagents.ext.llm.openai import create_openai_agent
 from miniagents.miniagents import MiniAgents
-from miniagents.promising.sentinels import AWAIT
-from miniagents.utils import achain_loop
+from miniagents.utils import adialog_loop
 
 load_dotenv()
 
@@ -24,12 +23,9 @@ async def amain() -> None:
     """
     try:
         print()
-        await achain_loop(
-            [
-                create_console_user_agent(chat_history_md_file=CHAT_MD_FILE),
-                AWAIT,  # TODO Oleksandr: explain this with an inline comment like this one
-                create_openai_agent(model="gpt-4o-2024-05-13"),
-            ]
+        await adialog_loop(
+            user_agent=create_console_user_agent(chat_history_md_file=CHAT_MD_FILE),
+            assistant_agent=create_openai_agent(model="gpt-4o-2024-05-13"),
         )
     except KeyboardInterrupt:
         print()
