155f2bd774ed3bc4c8cdedc310a2fce14fa08b57
Oleksandr Tereshchenko <toporok@gmail.com>
2024-06-24 14:50:04 +0300

    in progress: everything is an agent

--- examples/conversation.py
+++ examples/conversation.py
@@ -6,7 +6,7 @@ import logging
 
 from dotenv import load_dotenv
 
-from miniagents.ext.agent_aggregators import dialog_loop_agent
+from miniagents.ext.agent_aggregators import dialog_loop
 from miniagents.ext.chat_history_md import ChatHistoryMD
 from miniagents.ext.llm.openai import openai_agent
 from miniagents.ext.user_agents import console_user_agent
@@ -22,7 +22,7 @@ async def amain() -> None:
     chat_history = ChatHistoryMD("CHAT.md")
     try:
         print()
-        await dialog_loop_agent.fork(
+        await dialog_loop.fork(
             user_agent=console_user_agent.fork(chat_history=chat_history),
             assistant_agent=openai_agent.fork(model="gpt-4o-2024-05-13"),
         ).inquire()
--- miniagents/ext/agent_aggregators.py
+++ miniagents/ext/agent_aggregators.py
@@ -10,7 +10,7 @@ from miniagents.promising.sentinels import Sentinel, AWAIT, CLEAR
 
 
 @miniagent
-async def dialog_loop_agent(
+async def dialog_loop(
     ctx: InteractionContext,
     user_agent: Union[MiniAgent, Sentinel],
     assistant_agent: Union[MiniAgent, Sentinel],
@@ -19,7 +19,7 @@ async def dialog_loop_agent(
     Run a loop that chains the user agent and the assistant agent in a dialog.
     """
     ctx.reply(
-        chain_loop_agent.fork(
+        agent_loop.fork(
             agents=[
                 user_agent,
                 AWAIT,  # TODO Oleksandr: explain this with an inline comment like this one
@@ -30,7 +30,7 @@ async def dialog_loop_agent(
 
 
 @miniagent
-async def chain_loop_agent(ctx: InteractionContext, agents: Iterable[Union[MiniAgent, Sentinel]]) -> None:
+async def agent_loop(ctx: InteractionContext, agents: Iterable[Union[MiniAgent, Sentinel]]) -> None:
     """
     An agent that represents a loop that chains the given agents together in the order they are provided.
     """
@@ -43,20 +43,36 @@ async def chain_loop_agent(ctx: InteractionContext, agents: Iterable[Union[MiniA
 
     messages = ctx.messages
     while True:
-        for agent in agents:
-            if agent is None:
-                # TODO Oleksandr: explain
-                continue
+        messages = await _achain_agents(agents, messages)
+        # TODO Oleksandr: How should the agents end the loop ? What message sequence should be returned
+        #  when the loop is over ?
 
-            if agent is AWAIT:
-                if isinstance(messages, MessageSequencePromise):
-                    # all the interactions happen here (here all the scheduled promises are awaited for)
-                    messages = await messages.aresolve_messages()
-            elif agent is CLEAR:
-                messages = None
-            elif isinstance(agent, MiniAgent):
-                messages = agent.inquire(messages)
-            else:
-                raise ValueError(f"Invalid agent: {agent}")
-    # TODO Oleksandr: How should agents end the loop ? What message sequence should this utility return when the
-    #  loop is over ?
+
+@miniagent
+async def agent_chain(ctx: InteractionContext, agents: Iterable[Union[MiniAgent, Sentinel]]) -> None:
+    """
+    TODO Oleksandr: docstring
+    """
+    ctx.reply(await _achain_agents(list(agents), ctx.messages))
+
+
+async def _achain_agents(
+    agents: Iterable[Union[MiniAgent, Sentinel]], initial_messages: MessageSequencePromise
+) -> MessageSequencePromise:
+    messages = initial_messages
+    for agent in agents:
+        if agent is None:
+            # TODO Oleksandr: explain
+            continue
+
+        if agent is AWAIT:
+            if isinstance(messages, MessageSequencePromise):
+                # all the interactions happen here (here all the scheduled promises are awaited for)
+                messages = await messages.aresolve_messages()
+        elif agent is CLEAR:
+            messages = None
+        elif isinstance(agent, MiniAgent):
+            messages = agent.inquire(messages)
+        else:
+            raise ValueError(f"Invalid agent: {agent}")
+    return messages
