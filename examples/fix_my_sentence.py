66a6aa9eeaed3a5ec6672ff50159d176e8a9a4f9
Oleksandr Tereshchenko <toporok@gmail.com>
2024-06-25 21:00:46 +0300

    examples/fix_my_sentence.py

--- examples/fix_my_sentence.py
+++ examples/fix_my_sentence.py
@@ -7,8 +7,8 @@ import logging
 from dotenv import load_dotenv
 
 from miniagents.ext.agent_aggregators import dialog_loop
-from miniagents.ext.llm.anthropic import anthropic_agent
 from miniagents.ext.llm.llm_common import SystemMessage
+from miniagents.ext.llm.openai import openai_agent
 from miniagents.miniagents import MiniAgents
 
 load_dotenv()
@@ -19,7 +19,8 @@ async def amain() -> None:
     The main conversation loop.
     """
     await dialog_loop.fork(
-        assistant_agent=anthropic_agent.fork(model="claude-3-5-sonnet-20240620", max_tokens=1000)
+        # assistant_agent=anthropic_agent.fork(model="claude-3-5-sonnet-20240620", max_tokens=1000)
+        assistant_agent=openai_agent.fork(model="gpt-4o-2024-05-13")
     ).inquire(
         SystemMessage("Your job is to improve the styling and grammar of the sentences that the user throws at you.")
     )
