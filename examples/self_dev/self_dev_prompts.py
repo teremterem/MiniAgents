ec13a1a303919ddbb29597727f569792864b61d0
Oleksandr Tereshchenko <toporok@gmail.com>
2024-06-25 15:22:17 +0300

    readme_agent.py

--- examples/self_dev/self_dev_prompts.py
+++ examples/self_dev/self_dev_prompts.py
@@ -6,7 +6,14 @@ SYSTEM_HERE_ARE_REPO_FILES = (
     "Here are the source files of a Python framework that I'm building that goes by the name of MiniAgents."
 )
 SYSTEM_IMPROVE_README = (
-    "Please improve the README.md in this framework (take care of TODOs, improve the explanations, get rid of "
-    "repetitions, fix code examples; pay attention to the source code files of the framework itself when you do "
-    "all that)."
+    "Please improve the README.md in this framework:\n"
+    "\n"
+    "1) Take care of TODOs\n"
+    "2) Improve descriptions of things, where necessary\n"
+    "3) Get rid of repetitions\n"
+    "4) Fix code examples if broken\n"
+    "5) Do everything the user asks for\n"
+    "\n"
+    "Make sure to pay a lot of attention to the framework's source code while doing all of the above, "
+    "because the current version(s) of README.md might not be up to date or might contain errors."
 )
