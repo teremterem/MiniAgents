4be39293f229ada135a2cf33305035e175c55f9c
Oleksandr Tereshchenko <toporok@gmail.com>
2024-06-29 15:59:19 +0300

    list more models in self_dev_common.py

--- examples/self_dev/self_dev_common.py
+++ examples/self_dev/self_dev_common.py
@@ -17,9 +17,11 @@ MAX_OUTPUT_TOKENS = 4096
 
 MODEL_AGENT_FACTORIES = {
     "gpt-4o-2024-05-13": openai_agent.fork(temperature=0),
+    "gpt-4-turbo-2024-04-09": openai_agent.fork(temperature=0),
+    "gpt-3.5-turbo-0125": openai_agent.fork(temperature=0),
     "claude-3-5-sonnet-20240620": anthropic_agent.fork(max_tokens=MAX_OUTPUT_TOKENS, temperature=0),
     "claude-3-opus-20240229": anthropic_agent.fork(max_tokens=MAX_OUTPUT_TOKENS, temperature=0),
-    # "claude-3-haiku-20240307": anthropic_agent.fork(max_tokens=MAX_OUTPUT_TOKENS, temperature=0),
+    "claude-3-haiku-20240307": anthropic_agent.fork(max_tokens=MAX_OUTPUT_TOKENS, temperature=0),
 }
 MODEL_AGENTS = {model: agent.fork(model=model) for model, agent in MODEL_AGENT_FACTORIES.items()}
 
