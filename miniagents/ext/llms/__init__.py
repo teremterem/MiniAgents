a56f5069189a9692183ed928263cc998ef77eac1
Oleksandr Tereshchenko <toporok@gmail.com>
2025-04-01 03:09:22 +0300

    Support WebResearch example (#114)
    * aprepare_dicts_for_openai and aprepare_dicts_for_anthropic utility functions
    
    * some work to properly support automatic prompt caching
    
    * minor
    
    * minor

--- miniagents/ext/llms/__init__.py
+++ miniagents/ext/llms/__init__.py
@@ -1,6 +1,13 @@
-from miniagents.ext.llms.anthropic import AnthropicAgent, AnthropicMessage
-from miniagents.ext.llms.llm_utils import AssistantMessage, LLMAgent, LLMMessage, SystemMessage, UserMessage
-from miniagents.ext.llms.openai import OpenAIAgent, OpenAIMessage, openai_embedding_agent
+from miniagents.ext.llms.anthropic import AnthropicAgent, AnthropicMessage, aprepare_dicts_for_anthropic
+from miniagents.ext.llms.llm_utils import (
+    AssistantMessage,
+    LLMAgent,
+    LLMMessage,
+    SystemMessage,
+    UserMessage,
+    message_to_llm_dict,
+)
+from miniagents.ext.llms.openai import OpenAIAgent, OpenAIMessage, aprepare_dicts_for_openai, openai_embedding_agent
 
 __all__ = [
     "AnthropicAgent",
@@ -8,6 +15,9 @@ __all__ = [
     "AssistantMessage",
     "LLMAgent",
     "LLMMessage",
+    "aprepare_dicts_for_anthropic",
+    "aprepare_dicts_for_openai",
+    "message_to_llm_dict",
     "openai_embedding_agent",
     "OpenAIAgent",
     "OpenAIMessage",
