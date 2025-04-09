1323a4200c9ea8799fdf645d51413989f350f87a
Oleksandr Tereshchenko <toporok@gmail.com>
2025-04-09 12:05:48 +0300

    append-unordered - unit tests

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -107,13 +107,13 @@ async def test_sub_agents_run_in_parallel(start_soon: Union[bool, Sentinel]) -> 
         ]
 
 
-@pytest.mark.parametrize("start_soon", [False, True, None])
-async def test_agents_reply_unordered(start_soon: Union[bool, Sentinel]) -> None:
+@pytest.mark.parametrize("start_everything_soon_by_default", [False, True])
+async def test_agents_reply_unordered(start_everything_soon_by_default: Union[bool, Sentinel]) -> None:
     @miniagent
     async def agent1(ctx: InteractionContext) -> None:
         ctx.reply("agent1 msg1")
         ctx.reply("agent1 msg2")
-        ctx.reply(agent2.trigger(start_soon=start_soon))
+        ctx.reply(agent2.trigger())
         ctx.reply("agent1 msg3")
         ctx.reply("agent1 msg4")
 
@@ -122,7 +122,7 @@ async def test_agents_reply_unordered(start_soon: Union[bool, Sentinel]) -> None
         ctx.reply_unordered("agent2 msg1 pre-sleep unordered")
         ctx.reply_unordered("agent2 msg2 pre-sleep unordered")
         await asyncio.sleep(0.1)
-        ctx.reply(agent3.trigger(start_soon=start_soon))
+        ctx.reply(agent3.trigger())
         await asyncio.sleep(0.1)
         ctx.reply_unordered("agent2 msg3 post-sleep unordered")
         ctx.reply_unordered("agent2 msg4 post-sleep unordered")
@@ -131,7 +131,7 @@ async def test_agents_reply_unordered(start_soon: Union[bool, Sentinel]) -> None
     async def agent3(ctx: InteractionContext) -> None:
         ctx.reply_unordered("agent3 msg1 pre-sleep unordered")
         ctx.reply_unordered("agent3 msg2 pre-sleep unordered")
-        ctx.reply_unordered(agent4.trigger(start_soon=start_soon))
+        ctx.reply_unordered(agent4.trigger())
         await asyncio.sleep(0.2)
         ctx.reply_unordered("agent3 msg3 post-sleep unordered")
         ctx.reply_unordered("agent3 msg4 post-sleep unordered")
@@ -142,11 +142,11 @@ async def test_agents_reply_unordered(start_soon: Union[bool, Sentinel]) -> None
         ctx.reply_unordered("agent4 msg1 post-sleep unordered")
         ctx.reply_unordered("agent4 msg2 post-sleep unordered")
 
-    async with MiniAgents():
-        replies = await agent1.trigger(start_soon=start_soon)
+    async with MiniAgents(start_everything_soon_by_default=start_everything_soon_by_default):
+        replies = await agent1.trigger()
         replies = [reply.content for reply in replies]
 
-    if start_soon in [True, None]:
+    if start_everything_soon_by_default in [True, None]:
         # `start_soon` is True by default in `MiniAgents()`
         assert replies == [
             "agent1 msg1",
@@ -183,13 +183,16 @@ async def test_agents_reply_unordered(start_soon: Union[bool, Sentinel]) -> None
         ]
 
 
-@pytest.mark.parametrize("start_soon", [False, True, None])
-async def test_agents_reply_unordered_exception(start_soon: Union[bool, Sentinel]) -> None:
+@pytest.mark.parametrize("start_everything_soon_by_default", [False, True])
+@pytest.mark.parametrize("errors_to_messages", [False, True])
+async def test_agents_reply_unordered_exception(
+    start_everything_soon_by_default: Union[bool, Sentinel], errors_to_messages: bool
+) -> None:
     @miniagent
     async def agent1(ctx: InteractionContext) -> None:
         ctx.reply("agent1 msg1")
         ctx.reply("agent1 msg2")
-        ctx.reply(agent2.trigger(start_soon=start_soon))
+        ctx.reply(agent2.trigger(errors_to_messages=errors_to_messages))
         ctx.reply("agent1 msg3")
         ctx.reply("agent1 msg4")
 
@@ -198,7 +201,7 @@ async def test_agents_reply_unordered_exception(start_soon: Union[bool, Sentinel
         ctx.reply_unordered("agent2 msg1 pre-sleep unordered")
         ctx.reply_unordered("agent2 msg2 pre-sleep unordered")
         await asyncio.sleep(0.1)
-        ctx.reply(agent3.trigger(start_soon=start_soon))
+        ctx.reply(agent3.trigger(errors_to_messages=errors_to_messages))
         await asyncio.sleep(0.1)
         ctx.reply_unordered("agent2 msg3 post-sleep unordered")
         ctx.reply_unordered("agent2 msg4 post-sleep unordered")
@@ -207,7 +210,7 @@ async def test_agents_reply_unordered_exception(start_soon: Union[bool, Sentinel
     async def agent3(ctx: InteractionContext) -> None:
         ctx.reply_unordered("agent3 msg1 pre-sleep unordered")
         ctx.reply_unordered("agent3 msg2 pre-sleep unordered")
-        ctx.reply_unordered(agent4.trigger(start_soon=start_soon))
+        ctx.reply_unordered(agent4.trigger(errors_to_messages=errors_to_messages))
         await asyncio.sleep(0.2)
         ctx.reply_unordered("agent3 msg3 post-sleep unordered")
         ctx.reply_unordered("agent3 msg4 post-sleep unordered")
@@ -220,15 +223,21 @@ async def test_agents_reply_unordered_exception(start_soon: Union[bool, Sentinel
         await asyncio.sleep(0.1)  # TODO figure out why the previous two replies are lost if this sleep is removed
         raise ValueError("agent4 exception")
 
-    async with MiniAgents():
-        replies = []
-        with pytest.raises(ValueError):
-            async for reply_promise in agent1.trigger(start_soon=start_soon):
-                replies.append((await reply_promise).content)
+    async with MiniAgents(start_everything_soon_by_default=start_everything_soon_by_default):
+        reply_promises = agent1.trigger(errors_to_messages=errors_to_messages)
 
-    if start_soon in [True, None]:
+        if errors_to_messages:
+            actual_replies = await reply_promises
+            actual_replies = [reply.content for reply in actual_replies]
+        else:
+            actual_replies = []
+            with pytest.raises(ValueError):
+                async for reply_promise in reply_promises:
+                    actual_replies.append((await reply_promise).content)
+
+    if start_everything_soon_by_default in [True, None]:
         # `start_soon` is True by default in `MiniAgents()`
-        assert replies == [
+        expected_replies = [
             "agent1 msg1",
             "agent1 msg2",
             "agent2 msg1 pre-sleep unordered",
@@ -241,11 +250,17 @@ async def test_agents_reply_unordered_exception(start_soon: Union[bool, Sentinel
             "agent3 msg4 post-sleep unordered",
             "agent4 msg1 post-sleep unordered",
             "agent4 msg2 post-sleep unordered",
-            # "agent1 msg3",
-            # "agent1 msg4",
         ]
+        if errors_to_messages:
+            expected_replies.extend(
+                [
+                    "agent4 exception",
+                    "agent1 msg3",
+                    "agent1 msg4",
+                ]
+            )
     else:
-        assert replies == [
+        expected_replies = [
             "agent1 msg1",
             "agent1 msg2",
             "agent2 msg1 pre-sleep unordered",
@@ -256,8 +271,16 @@ async def test_agents_reply_unordered_exception(start_soon: Union[bool, Sentinel
             "agent3 msg2 pre-sleep unordered",
             "agent4 msg1 post-sleep unordered",
             "agent4 msg2 post-sleep unordered",
-            # "agent3 msg3 post-sleep unordered",
-            # "agent3 msg4 post-sleep unordered",
-            # "agent1 msg3",
-            # "agent1 msg4",
         ]
+        if errors_to_messages:
+            expected_replies.extend(
+                [
+                    "agent4 exception",
+                    "agent3 msg3 post-sleep unordered",
+                    "agent3 msg4 post-sleep unordered",
+                    "agent1 msg3",
+                    "agent1 msg4",
+                ]
+            )
+
+    assert actual_replies == expected_replies
