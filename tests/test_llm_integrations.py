b4bb575e5971221db1bd96fd42006d262deb813f
Oleksandr Tereshchenko <toporok@gmail.com>
2025-05-19 12:34:06 +0300

    rename test_llms.py to test_llm_integrations.py

--- tests/test_llm_integrations.py
+++ tests/test_llm_integrations.py
@@ -38,7 +38,7 @@ def _check_anthropic_response(message: Message) -> None:
 @pytest.mark.parametrize("stream", [False, True])
 @pytest.mark.parametrize("start_soon", [False, True])
 @pytest.mark.parametrize("flip_consumption_order", [False, True])
-async def test_llm(
+async def test_llm_integration(
     llm_agent: MiniAgent,
     check_response_func: Callable[[Message], None],
     stream: bool,
@@ -46,7 +46,7 @@ async def test_llm(
     flip_consumption_order: bool,
 ) -> None:
     """
-    Assert that all the LLM agents can respond to a simple prompt.
+    Assert that all the LLM agents respond to a simple prompt properly.
     """
     async with MiniAgents(start_everything_soon_by_default=start_soon):
         reply_sequence = llm_agent.trigger(
