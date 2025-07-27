9fd9eeaa9f6e4d93e92db2ed6257e6b1ff672ab2
Oleksandr Tereshchenko <toporok@gmail.com>
2025-07-27 17:34:41 +0300

    [MINOR] Introduce `miniagents/promising/promise_utils.py`; read `FROZEN_LONGER_HASH_KEYS` env var (#172)
    * uv lock --upgrade
    
    * stop serializing sub-message hashes (for now)
    
    * minor
    
    * allow promises inside Frozen; always store nested messages as MessagePromise
    
    * move `cached_privately` to `promise_utils.py`; attempt to add custom `pydantic` serialization to `Promise`
    
    * revert the first Promise in Frozen attempt
    
    * in progress: support Promise inside Frozen
    
    * prepare to serialize Promise inside Frozen
    
    * introduce FROZEN_LONGER_HASH_KEYS env var
    
    * type hints for _pydantic_serialize
    
    * revert my attempt to implement Promise inside Message for now; uv lock --upgrade
    
    * revert my attempt to implement Promise inside Message for now

--- miniagents/messages.py
+++ miniagents/messages.py
@@ -12,7 +12,8 @@ from pydantic import BaseModel
 
 from miniagents.miniagent_typing import MessageTokenStreamer, MessageType
 from miniagents.promising.errors import AppenderNotOpenError, PromisingContextError
-from miniagents.promising.ext.frozen import Frozen, StrictFrozen, cached_privately
+from miniagents.promising.ext.frozen import Frozen, StrictFrozen
+from miniagents.promising.promise_utils import cached_privately
 from miniagents.promising.promising import _StreamReplayIterator, StreamAppender, StreamedPromise
 from miniagents.promising.sentinels import NO_VALUE, Sentinel
 from miniagents.promising.sequence import FlatSequence
